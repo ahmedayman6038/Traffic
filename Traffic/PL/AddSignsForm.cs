d1fc4fc7e91b279d0c7629705182b85fae948ea0
ahmedayman6038 <32771157+ahmedayman6038@users.noreply.github.com>
2018-07-06 17:21:20 +0200

    errorSolved

--- Traffic/PL/AddSignsForm.cs
+++ Traffic/PL/AddSignsForm.cs
@@ -27,11 +27,15 @@ namespace Traffic.PL
 
         private void close_Click(object sender, EventArgs e)
         {
-           TrafficBoard temp = new TrafficBoard();
+            TrafficBoard temp = new TrafficBoard();
             for (int rows = 0; rows < signs.Rows.Count - 1; rows++)
             {
-                temp.point1.name = signs.Rows[rows].Cells[0].Value.ToString();
-                temp.point2.name = signs.Rows[rows].Cells[1].Value.ToString();
+                Point1 pt1 = new Point1();
+                pt1.name = signs.Rows[rows].Cells[0].Value.ToString();
+                temp.point1 = pt1;
+                Point2 pt2 = new Point2();
+                pt2.name = signs.Rows[rows].Cells[1].Value.ToString();
+                temp.point2 = pt2;
                 temp.boardNumber = Int32.Parse(signs.Rows[rows].Cells[2].Value.ToString());
 
                 boards.Add(temp);
